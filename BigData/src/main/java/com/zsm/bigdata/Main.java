4a5749fcef38d5b159cd2302b07c914293db7dc7
zengsm <453752303@qq.com>
2018-03-03 08:48:31 +0800

    清理Main函数无效代码

---
+++ BigData/src/main/java/com/zsm/bigdata/Main.java
@@ -0,0 +1,16 @@
+package com.zsm.bigdata;
+
+/**
+ * @Author: zengsm.
+ * @Description:
+ * @Date:Created in 2018/2/6 16:15.
+ * @Modified By:
+ */
+public class Main
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+
+    }
+}
