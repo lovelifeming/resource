9f8761ad4591db1f6cd14849e2c3c928397d83d5
Shengming <453752303@qq.com>
2018-09-01 18:02:45 +0800

    Optimize code and add dependencies.

--- SpringBoot/SpringBootDemo/src/main/java/com/zsm/sb/Swagger2.java
+++ SpringBoot/SpringBootDemo/src/main/java/com/zsm/sb/Swagger2.java
@@ -16,6 +16,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * swagger2的配置文件，建在项目的启动类的同级目录下,访问路径：http://localhost:8080/swagger-ui.html
  *
+ * @Api：修饰整个类，描述Controller的作用
+ * @ApiOperation：描述一个类的一个方法，或者说一个接口
+ * @ApiParam：单个参数描述
+ * @ApiModel：用对象来接收参数
+ * @ApiProperty：用对象接收参数时，描述对象的一个字段
+ * @ApiResponse：HTTP响应其中1个描述
+ * @ApiResponses：HTTP响应整体描述
+ * @ApiIgnore：使用该注解忽略这个API
+ * @ApiClass
+ * @ApiError
+ * @ApiErrors
+ * @ApiParamImplicit
+ * @ApiParamsImplicit
+ * @-------------------------------
  * @Author: zengsm.
  * @Description:
  * @Date:Created in 2018/4/11.
