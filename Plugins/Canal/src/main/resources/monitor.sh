9321bd5e7d4828766d10c3c7b0a8d067bba8c18b
zengsm <453752303@qq.com>
2020-03-31 09:45:41 +0800

    添加大数据组件

--- Plugins/Canal/src/main/resources/monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-JarName=canal.jar
-   
-isRunning=$(ps -ef | grep $JarName | grep -v grep)    
-if [ "$isRunning" ] ; then   
-    echo "$JarName is running ~~~"   
-else   
-    echo "$JarName 异常" | mailx -r monitor -s "$JarName ERROR" test@qq.com
-fi
